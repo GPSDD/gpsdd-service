abf3460ce2aa21e1030cd73e8c5ced5e979c6a6d
Dan Hammer <dan.s.hammer@gmail.com>
2016-03-23 22:31:35 -0700

    initial commit

---
+++ api.py
@@ -0,0 +1,27 @@
+import json
+import povcal
+import webapp2
+
+
+class PovcalHandler(webapp2.RequestHandler):
+    def get(self):
+
+        # headers
+        self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+        self.response.headers['Content-Type'] = 'application/json'
+
+        # parameters
+        country = self.request.get('country')
+        povline = float(self.request.get('povline'))
+        proportion = bool(self.request.get('proportion', False))
+
+        # response
+        res = povcal.constructor(country, povline, proportion=proportion)
+        self.response.write(json.dumps(res))
+
+
+handlers = webapp2.WSGIApplication(
+    [
+        ('/povcal', PovcalHandler)
+    ], debug=True
+)
---
+++ povcal.py
@@ -0,0 +1,46 @@
+import numpy as np
+import urllib
+from google.appengine.api import urlfetch
+
+
+def constructor(country, povline, proportion=True):
+
+    base = "http://iresearch.worldbank.org/PovcalNet/PovcalNetAPI.ashx"
+    yearlist = ','.join([str(x) for x in np.arange(1970, 2018)])
+
+    payload = {
+        "Y0": yearlist,
+        "PL0": povline,
+        "C0": country + "_3",
+        "GroupedBy": "false"
+    }
+
+    url = base + "?" + urllib.urlencode(payload)
+    data = urlfetch.fetch(url=url).content.split("\n")
+    header, items = data[0], data[1:]
+
+    header = [x.strip("'") for x in header.split("\t")]
+
+    res = []
+    for xx in [i.split("\t") for i in items]:
+        values = [x.strip("'") for x in xx]
+        d = dict(zip(header, values))
+        try:
+            print d
+            pop = int(float(d["ReqYearPopulation"])*1000000)
+            res += [{
+                "year": int(d["RequestYear"]),
+                "population": pop,
+                "impoverished": int(pop*float(d["HeadCount"]))
+            }]
+
+        except Exception, e:
+            print e
+            pass
+
+    return dict(
+        country=country,
+        count=len(res),
+        results=res,
+        poverty_line=povline
+    )
